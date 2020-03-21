571211d1691283fbffbe9a5ac6beea9dad54b9d3
Seza Demirbaş <38151013+majorisha7@users.noreply.github.com>
2020-03-21 21:15:47 +0300

    changed model&service and added employee impl test

--- src/main/java/com/datassist/spring/models/Employee.java
+++ src/main/java/com/datassist/spring/models/Employee.java
@@ -3,9 +3,11 @@ package com.datassist.spring.models;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
 import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Data
 @Document(collection = "Employee")
@@ -51,5 +53,23 @@ public class Employee {
 
     private String salaryPeriod;
 
+    public Employee fromUpdateDTO(EmployeeUpdateDTO dto) {
+        firstName = Optional.ofNullable(dto.getFirstName()).orElse(this.firstName);
+        lastName = Optional.ofNullable(dto.getLastName()).orElse(this.lastName);
+        age = dto.getAge() == 0 ? this.age : dto.getAge();
+        childCount = dto.getChildCount() == 0 ? this.age : dto.getChildCount();
+        citizenship = Optional.ofNullable(dto.getCitizenship()).orElse(this.citizenship);
+        employeePhoto = Optional.ofNullable(dto.getEmployeePhoto()).orElse(this.employeePhoto);
+        employer = Optional.ofNullable(dto.getEmployer()).orElse(this.employer);
+        hireDate = Optional.ofNullable(dto.getHireDate()).orElse(this.getHireDate());
+        gender = Optional.ofNullable(dto.getGender()).orElse(this.gender);
+        maritalStatus = Optional.ofNullable(dto.getMaritalStatus()).orElse(this.maritalStatus);
+        profession = Optional.ofNullable(dto.getProfession()).orElse(this.profession);
+        salary = Optional.ofNullable(dto.getSalary()).orElse(this.salary);
+        salaryCurrency = Optional.ofNullable(dto.getSalaryCurrency()).orElse(this.salaryCurrency);
+        salaryPeriod = Optional.ofNullable(dto.getSalaryPeriod()).orElse(this.salaryPeriod);
+        retired = Optional.of(dto.isRetired()).orElse(this.retired);
+        return this;
+    }
 
 }
